a3f38e194cc2033349098b889b31323bdab3df08
Jordan Baczuk <jbaczuk@robinsage.com>
2018-12-10 16:00:46 +0000

    Update code licenses and copyrights. Fixes #37

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2014-2018 The Compchain Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2018 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
