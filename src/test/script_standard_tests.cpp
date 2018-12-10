4f1e667554ec2904df2f093d8a4fd1c9e2e79761
Jordan Baczuk <jbaczuk@robinsage.com>
2018-12-10 16:00:46 +0000

    Update code licenses and copyrights. Fixes #37

--- src/test/script_standard_tests.cpp
+++ src/test/script_standard_tests.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2017-2018 The Compchain Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 // Copyright (c) 2017-2018 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
